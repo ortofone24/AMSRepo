905bfb207cb31fc7da498c92d4de3460a8c559c2
ortofone24 <ortofone24@gmail.com>
2021-04-15 07:26:35 +0200

    Add files via upload

---
+++ Ams.Api/Ams.Api/Services/IUriService.cs
@@ -0,0 +1,13 @@
+﻿using Ams.Api.Filter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ams.Api.Services
+{
+    public interface IUriService
+    {
+        public Uri GetPageUri(PaginationFilter filter, string route);
+    }
+}
---
+++ Ams.Api/Ams.Api/Wrappers/Response.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ams.Api.Wrappers
+{
+    public class Response<T>
+    {
+        public Response(T data)
+        {
+            Succeeded = true;
+            Message = string.Empty;
+            Errors = null;
+            Data = data;
+        }
+        public T Data { get; set; }
+        public bool Succeeded { get; set; }
+        public string[] Errors { get; set; }
+        public string Message { get; set; }
+    }
+}
